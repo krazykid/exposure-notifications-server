251fd88a8764e9870c8fe55c438e7d31e599af70
Ernest Kim <krazykid@gmail.com>
2020-09-14 12:47:35 -0400

    Updating DO-FIRST-MITRE

--- terraform/DO-FIRST-MITRE/variables.tf
+++ terraform/DO-FIRST-MITRE/variables.tf
@@ -1,7 +1,8 @@
 variable "env_project_id_map" {
-  type = "map"
+  type = map(string)
   default = {
     "developer-jmazella" = "mitre-pact-jmazella"
+    "testing-qwerty" = "mitre-pact-testing-qwerty"
   }
 }
 
@@ -17,4 +18,19 @@ locals {
   subnet_us_east4_cidr = lookup(var.env_subnet_us_east4_cidr_map, terraform.workspace, "10.8.56.0/24")
   region = "us-east4"
 
+  c19_dev_roles = [
+    "roles/browser",
+    "roles/cloudbuild.builds.editor",
+    "roles/cloudkms.publicKeyViewer",
+    "roles/cloudscheduler.admin",
+    "roles/cloudsql.admin",
+    "roles/cloudsql.client",
+    "roles/editor",
+    "roles/iam.serviceAccountUser",
+    "roles/run.admin",
+    "roles/secretmanager.admin",
+    "roles/secretmanager.secretAccessor",
+    "roles/storage.admin",
+    "roles/viewer",
+  ]
 }
